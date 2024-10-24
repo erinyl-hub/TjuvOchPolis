1e474523900b2787061e697e37dccea906c1ad35
Felicia Runfrost <Felicia.Runfrost@hotmail.com>
2024-10-24 11:31:02 +0200

    Nytt förllag för loopar

--- Metoder.cs
+++ Metoder.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace TjuvOchPolis
 {
@@ -12,72 +13,49 @@ namespace TjuvOchPolis
         {
             Name names = new Name();
 
-            for (int i = 0; i < 10; i++) // Gör till en metod
+            
+            AddPersonToList(citizens, 10, names.polisnamn.ToList(), typeof(Polis)); 
+            AddPersonToList(citizens, 20, names.tjuvnamn.ToList(), typeof(Tjuv));   
+            AddPersonToList(citizens, 30, names.medborgarnamn.ToList(), typeof(Medborgare)); 
+        }
+
+        public void AddPersonToList(List<Person> citizens, int antal, List<string> namnlista, Type typ)
+        {
+            for (int i = 0; i < antal; i++)
             {
                 int x;
                 int y;
                 bool loopKey = false;
+
+                // Hitta en giltig position
                 do
                 {
                     x = Random.Shared.Next(0, 101);
                     y = Random.Shared.Next(0, 26);
                     loopKey = PositionTest(citizens, x, y);
+
                 } while (loopKey == false);
 
-
-                string name = names.polisnamn[i];
+                // Slumpa fram ett namn och rörelse
+                string name = namnlista[i];
                 int movement = Random.Shared.Next(0, 7);
 
-                Polis polis = new Polis(x, y, name, movement);
-
-
-                citizens.Add(polis);
-            }
-
-            for (int i = 0; i < 20; i++)
-            {
-                int x;
-                int y;
-                bool loopKey = false;
-                do
+                // Skapa rätt typ av person och lägg till i listan
+                if (typ == typeof(Polis))
                 {
-                    x = Random.Shared.Next(0, 101);
-                    y = Random.Shared.Next(0, 26);
-                    loopKey = PositionTest(citizens, x, y);
-                } while (loopKey == false);
-
-
-                string name = names.tjuvnamn[i];
-                int movement = Random.Shared.Next(0, 7);
-
-                Tjuv tjuv = new Tjuv(x, y, name, movement, true);
-
-
-                citizens.Add(tjuv);
-            }
-
-
-            for (int i = 0; i < 30; i++)
-            {
-                int x;
-                int y;
-                bool loopKey = false;
-                do
+                    Polis polis = new Polis(x, y, name, movement);
+                    citizens.Add(polis);
+                }
+                else if (typ == typeof(Tjuv))
                 {
-                    x = Random.Shared.Next(0, 101);
-                    y = Random.Shared.Next(0, 26);
-                    loopKey = PositionTest(citizens, x, y);
-                } while (loopKey == false);
-
-
-
-                string name = names.medborgarnamn[i];
-                int movement = Random.Shared.Next(0, 7);
-
-                Medborgare civilian = new Medborgare(x, y, name, movement);
-
-
-                citizens.Add(civilian);
+                    Tjuv tjuv = new Tjuv(x, y, name, movement, true);
+                    citizens.Add(tjuv);
+                }
+                else if (typ == typeof(Medborgare))
+                {
+                    Medborgare civilian = new Medborgare(x, y, name, movement);
+                    citizens.Add(civilian);
+                }
             }
         }
 
@@ -87,15 +65,12 @@ namespace TjuvOchPolis
             {
                 if (person.XPosition == x && person.YPosition == y)
                 {
-                    return false;
+                    return false; // Positionen är upptagen
                 }
-
-
-
             }
-            return true;
-
+            return true; // Positionen är ledig
         }
+
 
 
     }
