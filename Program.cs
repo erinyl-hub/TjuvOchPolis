4f3afa4272f5160d99f33ed5b95c8a9a1e80a1bb
Noel Burman <noelburman@hotmail.com>
2024-10-24 11:11:37 +0200

    Ritar ut bana och gubbar och rörelse

--- Program.cs
+++ Program.cs
@@ -10,32 +10,50 @@ namespace TjuvOchPolis
             List <Person> citizens = new List <Person> ();
             metoder.AddCitizens(citizens);
 
+                while (true) 
+                {
+                    Console.Clear(); 
 
-            
+                    
+                    TjuvOchPolis.Border();
+                    TjuvOchPolis.Bana(citizens);
+                    TjuvOchPolis.Border();
 
-            //foreach (Person person in citizens)
-            //{
-            //    
-            //    if (person is Polis polis)
-            //    {
-            //        Console.WriteLine($"{polis.Name} {polis.XPosition} {polis.YPosition} {polis.Riktning} {polis.Id}");
-            //    }
-            //    else if (person is Tjuv tjuv)
-            //    {
-            //        Console.WriteLine($"{tjuv.Name} {tjuv.XPosition} {tjuv.YPosition} {tjuv.Riktning} {tjuv.Id} {tjuv.Fri}");
-            //    }
-            //    else if (person is Medborgare civilian)
-            //    {
-            //        Console.WriteLine($"{civilian.Name} {civilian.XPosition} {civilian.YPosition} {civilian.Riktning} {civilian.Id}");
-            //    }
-            //}
+                   
+                    metoder.Rörelse(citizens);
+
+                    
+                    System.Threading.Thread.Sleep(500);
+                }
+          
 
 
-            TjuvOchPolis.Border();
-            TjuvOchPolis.Bana(citizens);
-            TjuvOchPolis.Border();
 
-            Console.ReadKey();
+
+
+        //foreach (Person person in citizens)
+        //{
+        //    
+        //    if (person is Polis polis)
+        //    {
+        //        Console.WriteLine($"{polis.Name} {polis.XPosition} {polis.YPosition} {polis.Riktning} {polis.Id}");
+        //    }
+        //    else if (person is Tjuv tjuv)
+        //    {
+        //        Console.WriteLine($"{tjuv.Name} {tjuv.XPosition} {tjuv.YPosition} {tjuv.Riktning} {tjuv.Id} {tjuv.Fri}");
+        //    }
+        //    else if (person is Medborgare civilian)
+        //    {
+        //        Console.WriteLine($"{civilian.Name} {civilian.XPosition} {civilian.YPosition} {civilian.Riktning} {civilian.Id}");
+        //    }
+        //}
+
+
+            //TjuvOchPolis.Border();
+            //TjuvOchPolis.Bana(citizens);
+            //TjuvOchPolis.Border();
+
+            //Console.ReadKey();
         }
     }
 }
