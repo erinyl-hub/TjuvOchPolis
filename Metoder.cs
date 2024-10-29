47e843f89e6acc516c822d93fa40ae250eeae07a
Felicia Runfrost <Felicia.Runfrost@hotmail.com>
2024-10-29 07:37:18 +0100

    Four new methods *HämtaTillåtnaRiktningar *SlumpaNyRiktning *FlyttaPerson *SkrivUtPersonMedFärg

--- Metoder.cs
+++ Metoder.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Linq;
 
 namespace TjuvOchPolis
 {
@@ -12,7 +11,6 @@ namespace TjuvOchPolis
         public void AddCitizens(List<Person> citizens)
         {
             Name names = new Name();
-
 
             AddPersonToList(citizens, 10, names.polisnamn.ToList(), typeof(Polis));
             AddPersonToList(citizens, 20, names.tjuvnamn.ToList(), typeof(Tjuv));
@@ -27,7 +25,7 @@ namespace TjuvOchPolis
                 int y;
                 bool loopKey = false;
 
-                // Hitta en giltig position
+                
                 do
                 {
                     x = Random.Shared.Next(1, 101);
@@ -36,11 +34,11 @@ namespace TjuvOchPolis
 
                 } while (loopKey == false);
 
-                // Slumpa fram ett namn och rörelse
+                
                 string name = namnlista[i];
                 int movement = Random.Shared.Next(0, 7);
 
-                // Skapa rätt typ av person och lägg till i listan
+                
                 if (typ == typeof(Polis))
                 {
                     Polis polis = new Polis(x, y, name, movement);
@@ -65,106 +63,115 @@ namespace TjuvOchPolis
             {
                 if (person.XPosition == x && person.YPosition == y)
                 {
-                    return false; // Positionen är upptagen
+                    return false; 
                 }
             }
-            return true; // Positionen är ledig
+            return true; 
         }
-
 
         public void Rörelse(List<Person> citizens)
         {
+            Random rand = new Random();
+
             foreach (Person person in citizens)
             {
-
+                
                 Console.SetCursorPosition(person.XPosition, person.YPosition);
                 Console.Write(" ");
 
+                
+                List<int> tillåtnaRiktningar = HämtaTillåtnaRiktningar(person.XPosition, person.YPosition); 
 
-                int direction = person.Riktning;
-
-                switch (direction)
+                
+                if (tillåtnaRiktningar.Count < 8) 
                 {
-                    case 0:
-                        if (person.YPosition > 1 && person.XPosition < 100)
-                            person.YPosition--;
-                        break;
-
-                    case 1:
-                        if (person.YPosition < 25) person.YPosition++;
-                        break;
-
-                    case 2:
-                        if (person.XPosition > 1) person.XPosition--;
-                        break;
-
-                    case 3:
-                        if (person.XPosition < 100) person.XPosition++;
-                        break;
-
-                    case 4:
-                        if (person.YPosition > 1 && person.XPosition > 1) // flyttar vänster uppåt
-                        { person.YPosition--; person.XPosition--; };
-                        break;
-
-                    case 5:
-                        if (person.YPosition < 25 && person.XPosition < 100) // flyttar höger neråt
-                        { person.YPosition++; person.XPosition++; };
-                        break;
-
-                    case 6:
-                        if (person.YPosition > 1 && person.XPosition < 100) // flyttar höger uppåt
-                        { person.YPosition--; person.XPosition++; };
-                        break;
-
-                    case 7:
-                        if (person.YPosition < 25 && person.XPosition > 1) // flyttar vänster neråt
-                        { person.YPosition++; person.XPosition--; };
-                        break;
+                    person.Riktning = SlumpaNyRiktning(tillåtnaRiktningar, rand); 
                 }
 
+                
+                FlyttaPerson(person); 
 
-                if (person is Polis)
-                {
-                    Console.ForegroundColor = ConsoleColor.Blue;  // har färgen kvar när den loopar
-                }
-                else if (person is Tjuv)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                }
-                else if (person is Medborgare)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                }
-
-
+                
                 Console.SetCursorPosition(person.XPosition, person.YPosition);
-                Console.Write(PersonMarkering(person));
-
-
-
-
-
-
+                SkrivUtPersonMedFärg(person); 
+                Console.ResetColor();
             }
+        }
 
+        // NY METOD: Hämta tillåtna riktningar beroende på position
+        private List<int> HämtaTillåtnaRiktningar(int x, int y)
+        {
+            List<int> tillåtnaRiktningar = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7 };
+
+            if (x <= 1)
+                tillåtnaRiktningar = new List<int> { 3, 5, 6 };
+
+            else if (x >= 100)
+                tillåtnaRiktningar = new List<int> { 2, 4, 7 };
+
+            else if (y <= 1)
+                tillåtnaRiktningar = new List<int> { 1, 5, 7 };
+
+            else if (y >= 25)
+                tillåtnaRiktningar = new List<int> { 0, 4, 6 };
+
+            return tillåtnaRiktningar;
+        }
+
+
+        // NY METOD: Slumpa en ny riktning från de tillåtna
+        private int SlumpaNyRiktning(List<int> tillåtnaRiktningar, Random rand)
+        {
+            return tillåtnaRiktningar[rand.Next(tillåtnaRiktningar.Count)];
+        }
+
+
+        // NY METOD: Flytta personen baserat på riktningen
+        private void FlyttaPerson(Person person)
+        {
+            switch (person.Riktning)
+            {
+                case 0: if (person.YPosition > 1) person.YPosition--; break;
+                case 1: if (person.YPosition < 25) person.YPosition++; break;
+                case 2: if (person.XPosition > 1) person.XPosition--; break;
+                case 3: if (person.XPosition < 100) person.XPosition++; break;
+                case 4: if (person.YPosition > 1 && person.XPosition > 1) { person.YPosition--; person.XPosition--; } break;
+                case 5: if (person.YPosition < 25 && person.XPosition < 100) { person.YPosition++; person.XPosition++; } break;
+                case 6: if (person.YPosition > 1 && person.XPosition < 100) { person.YPosition--; person.XPosition++; } break;
+                case 7: if (person.YPosition < 25 && person.XPosition > 1) { person.YPosition++; person.XPosition--; } break;
+            }
+        }
+
+
+        // NY METOD. Det är den gamla koden men i en metod istället
+        private void SkrivUtPersonMedFärg(Person person)
+        {
+            if (person is Polis)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            else if (person is Tjuv)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else if (person is Medborgare)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            Console.Write(PersonMarkering(person));
         }
 
         public char PersonMarkering(Person person)
         {
             if (person is Polis) return 'P';
-
-
             else if (person is Tjuv) return 'T';
-
-
             else if (person is Medborgare) return 'C';
 
             return ' ';
         }
-
     }
 }
+
 
 
 
